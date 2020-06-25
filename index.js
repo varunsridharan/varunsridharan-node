8894296ea306fe4ee0a152e9932c5306a73318b5
Varun <varunsridharan23@gmail.com>
2020-06-25 14:21:01 +0530

    Added Telegram

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 #!/usr/bin/env node
 
 const clearConsole = require( 'clear-any-console' );
-
-// Clears the console.
 clearConsole();
 
 console.log( `
 Varun Sridharan
----------------
+
 A self-taught person than a main stream academic, I started development with core PHP for development of websites and applications very early in life. 
 I was introduced to WordPress for the first time in 2004.
 
@@ -17,6 +15,7 @@ It has been an amazing 13-year journey with WordPress, and I look forward to mor
 > I ❤️ Open Source!
 
 🐦 Twitter: https://twitter.com/varunsridharan2
+📠 Telegram: https://go.svarun.dev/telegram/
 📖 GitHub:  https://github.com/varunsridharan
 😎 Website: https://varunsridharan.in
 ` );
