b5617fe9b8edff804b7edd39f701fd9d5a61c399
Varun Sridharan <varunsridharan23@gmail.com>
2020-06-25 22:37:35 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 #!/usr/bin/env node
 
-const clearConsole = require( 'clear-any-console' );
-clearConsole();
+const pkgJSON = require('./package.json');
+const welcome = require('cli-welcome');
+
+welcome({
+	title: pkgJSON.name,
+	tagLine: `Howdy, nice to meet ya!`,
+	description: pkgJSON.description,
+	version: pkgJSON.version,
+	bgColor: `#FADC00`,
+	color: `#000000`,
+	bold: true,
+	clear: true,
+});
 
 console.log( `
 Varun Sridharan
